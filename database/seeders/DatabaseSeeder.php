646d9221736625688dc51b3df6a760bb2201ba5b
Lorena Meci <lorena_meci.fshnstudent@unitir.edu.al>
2022-11-22 17:38:15 +0100

    first commit,breeze

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin', 
+            'email' => 'admin@gmail.com',
+            'phone' => '0668547122',
+            'password' => Hash::make('lorilori'),
+            'address' => 'Vale Drive Lorain, OH',
+            'role' => 'Admin',
+        ]);
+        
+        \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+       
+    }
+}
