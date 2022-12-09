5e82345300eef41a0edaba7e24f927db98d1ca62
Lorena Meci <lorena_meci.fshnstudent@unitir.edu.al>
2022-12-09 17:59:08 +0100

    last

--- app/Http/Controllers/NewPaswController.php
+++ app/Http/Controllers/NewPaswController.php
@@ -22,9 +22,6 @@ class NewPaswController extends Controller
     {
         $request->validated();
 
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
         $status = Password::reset(
             $request->only('email', 'password', 'password_confirmation', 'token'),
             function ($user) use ($request) {
@@ -36,10 +33,7 @@ class NewPaswController extends Controller
                 event(new PasswordReset($user));
             }
         );
-
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
+        
         return $status == Password::PASSWORD_RESET
                     ? redirect()->route('newlogin')->with('status', __($status))
                     : back()->withInput($request->only('email'))
